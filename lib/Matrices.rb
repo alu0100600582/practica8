4784c947ca83e212736e48004360fe2130f634f0
Francisco Mendoza <alu0100600582@ull.edu.es>
2013-11-06 10:54:25 +0000

    Añadiendo mas operaciones con matrices (multiplicacion y traspuesta)

--- lib/Matrices.rb
+++ lib/Matrices.rb
@@ -69,6 +69,72 @@ class Matriz
 		Matriz.new(@nfil, @ncol, elemento)
 	end
 	
+	#restar dos matrices
+	def -(other)
+		if ((@nfil != other.nfil) || (@ncol != other.ncol))
+			puts "No se pueden restar las matrices"
+		else
+			elemento = Array.new(0)
+			for i in 0...filas do
+				fila = Array.new(0)
+				for j in 0...colum do
+					fila << pos[i][j] - other.pos[i][j]
+				end
+				elemento << fila
+			end
+		end
+		Matriz.new(@nfil, @ncol, elemento)
+	end
+	
+	# multiplicar dos matrices
+	def por(other)
+		if ((@nfil != other.ncol) || (@ncol != other.nfil))
+			puts "No se pueden multiplicarr las matrices"
+		else
+			elemento = Array.new(0)
+			for i in 0...nfil do
+				fila = Array.new(0)
+				for j in 0...other.ncol do
+					aux = 0
+					for k in 0...ncol do
+						aux += pos[i][k] * other.pos[k][j]
+					end
+					fila << aux
+				end
+				elemento << fila
+			end
+		end
+		Matriz.new(@nfil, other.ncol, elemento)
+	end
+	
+	#multiplicar una matriz por un escalar
+	def *(other)
+		elemento = Array.new(0)
+		for i in 0...filas do
+			fila = Array.new(0)
+			for j in 0...colum do
+				fila << pos[i][j]*other
+			end
+			elemento << fila
+		end
+		Matriz.new(@nfil, @ncol, elemento)
+	end
+	
+	# hallar la traspuesta de una matriz
+	def trasponer
+		elemento = Array.new(0)
+		for i in 0...colum do
+			fila = Array.new(0)
+			for j in 0...filas do
+				fila << pos[j][i]
+			end
+			elemento << fila
+		end
+		Matriz.new(@ncol, @nfil, elemento)
+	end
+	
+	
+	
 end
 
 if __FILE__ == $0
