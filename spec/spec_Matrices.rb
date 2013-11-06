cba025ac279891db582f239fe511eabc2540d37c
Saray Diaz <alu0100600575@ull.edu.es>
2013-11-06 10:09:54 +0000

    Inicializando las pruebas para la clase Matriz

---
+++ spec/spec_Matrices.rb
@@ -0,0 +1,14 @@
+require "Matrices.rb"
+
+describe Matriz do
+	before :each do
+		@m1 = Matriz.new(2,2,[[1,2],[3,4]])
+		@m2 = Matriz.new(3,3,[[1,2,4],[6,5,8],[2,5,3]])
+		@m3 = Matriz.new(2,3,[[3,6,9],[2,8,4]])
+		@m4 = Matriz.new(3,2,[[1,3],[2,4],[6,5]])
+		@m5 = Matriz.new(2,2,[[5,3],[2,3]])
+	end
+	
+	
+	
+end
