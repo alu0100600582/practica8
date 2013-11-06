552927fabe335a0abdc86717f4ea9b713e3aa700
Saray Diaz <alu0100600575@ull.edu.es>
2013-11-06 10:17:03 +0000

    Añadiendo comprobacion de la creacion de una matriz

--- spec/spec_Matrices.rb
+++ spec/spec_Matrices.rb
@@ -9,6 +9,26 @@ describe Matriz do
 		@m5 = Matriz.new(2,2,[[5,3],[2,3]])
 	end
 	
-	
+	# Comprobacion de que se crea bien la matriz
+	describe "Comprobamos el numero de...\n" do
+		it " - Filas para una matriz 3x3\n" do
+			@m2.nfil.should eq(3)
+		end
+		it " - Columnas de una matriz 3x3\n" do
+			@m2.ncol.should eq(3)
+		end
+		it "Acceso al elemento [0][0]" do
+			@m1.pos[0][0].should eq(1)
+		end
+		it "Acceso al elemento [0][1]" do
+			@m1.pos[0][1].should eq(2)
+		end
+		it "Acceso al elemento [1][0]" do
+			@m1.pos[1][0].should eq(3)
+		end
+		it "Acceso al elemento [1][1]" do
+			@m1.pos[1][1].should eq(4)
+		end
+	end
 	
 end
